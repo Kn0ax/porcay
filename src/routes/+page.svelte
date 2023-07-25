d0b2ec5691232be3ef546c1af9fe15d57a04b824
Kax675 <kaxozae@gmail.com>
2023-07-25 16:49:49 +0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -56,7 +56,7 @@
         <br>
         <a class="button discord" href="https://discord.gg/ce8NdJ4FET">
             <img src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6ca814282eca7172c6_icon_clyde_white_RGB.svg">
-            Discord</a>
+            Discord</a> (Mobil cihazlar için kurulum Discord'da gösterilmiştir)
     </p>
 
     <div id="buttons">
